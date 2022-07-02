6c7d92b30426ef6fccfc42375d31ae9dc15531c1
NAhaile <nhaile2@masonlive.gmu.edu>
2022-07-02 02:31:35 -0400

    Tester update

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Cities');
+  const linkElement = screen.getByText('Hello World!');
   expect(linkElement).toBeInTheDocument();
 });
