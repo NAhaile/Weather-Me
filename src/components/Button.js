d3ff7aa8cc965fb6f13aee1ad03d76bb6439159e
NAhaile <nhaile2@masonlive.gmu.edu>
2022-07-02 18:06:05 -0400

    Updating to include Latitude and Longitude info

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 
+export default function Butto() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
 
-class Buttons extends React.Component {
-  render() {
-    return (
-      <div className = "Butt">
-        <button>Button 1</button>
-        <button>Button 2</button>
-        <button>Button 3</button>
-      </div>
-    );
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+  
+    console.log("Latitude is:", lat)
+    console.log("Longitude is:", long)
+  }, [lat, long]);
+
+  function AlertLatLong() {
+    alert("Latitude is " + lat + " Longitude is " + long);
   }
+ 
+  return (
+    <div className = "Butt">
+    <button onClick={AlertLatLong}>Button 1</button>
+    <button>Button 2</button>
+    <button>Button 3</button>
+    </div>
+  );
+
 }
 
-
-export default Buttons;
